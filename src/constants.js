8e8d3f7df9d9c367d7167e25c7c8a751cefdf9c9
Stephen Renda <stephenrendajr@gmail.com>
2020-05-31 12:00:29 -0700

    Updated with ES6 and hooks

---
+++ src/constants.js
@@ -0,0 +1,26 @@
+// Dimensions of the board (3x3 squares), game outcomes and players,
+// and dimensions for the board squares, in pixels.
+
+export const DIMS = 3;
+export const DRAW = 0;
+export const PLAYER_X = 1;
+export const PLAYER_O = 2;
+export const SQUARE_DIMS = 100;
+
+export const GAME_STATES = {
+  notStarted: "not_started",
+  inProgress: "in_progress",
+  over: "over",
+};
+
+export const GAME_MODES = {
+  easy: "easy",
+  medium: "medium",
+  difficult: "difficult",
+};
+
+export const SCORES = {
+  1: 1,
+  0: 0,
+  2: -1,
+};
